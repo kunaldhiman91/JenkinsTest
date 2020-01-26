6d5ca6adcd8beeab0c50ab80c015ee2fba6cffa0
Kunal Kumar <kunaldhiman91@gmail.com>
2020-01-25 21:05:09 -0500

    adding test for webhook test

--- JenkinsTest/JenkinsTest/ViewController.swift
+++ JenkinsTest/JenkinsTest/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.testJT()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func testJT() {
+        
     }
 
 
